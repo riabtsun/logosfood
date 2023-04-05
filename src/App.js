824f30bf40bdd2d4389ca1f9d8b87a2e9ad914ae
riabtsun <riabtsun@i.ua>
2023-04-05 16:59:47 +0300

    added router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './sass/style.sass'
 import Header from './Layout/Header/Header'
 import Begin from './pages/Home/Begin/Begin'
 import NavMenu from './components/NavMenu/NavMenu'
 import AllProducts from './pages/Home/AllProducts/AllProducts'
+import {BrowserRouter, Routes, Route, Link, useLocation} from "react-router-dom";
+import {useEffect} from "react";
+import './sass/style.sass'
+
 function App() {
+
+  // useEffect(()=>{
+  //   alert(` `)
+  // },[])
+
   return (
     <div className="App">
       <Header />
